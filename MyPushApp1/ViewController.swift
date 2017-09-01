eec581fafb729bf14db2773fbd27c5a6c1669ba0
Jonathan LEININGER <jonathan.leininger@open-groupe.com>
2017-09-01 08:53:20 +0200

    Added source code

---
+++ MyPushApp1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPushApp1
+//
+//  Created by Jonathan LEININGER on 31/08/2017.
+//  Copyright © 2017 open. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
